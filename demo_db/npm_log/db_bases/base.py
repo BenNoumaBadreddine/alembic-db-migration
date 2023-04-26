2414bb8a44a49097f97c133cb7441abbfba8ab4a
Badreddine <badreddine.ben.nouma@gmail.com>
2023-04-26 10:52:41 -0400

    update dependencies

--- demo_db/npm_log/db_bases/base.py
+++ demo_db/npm_log/db_bases/base.py
@@ -1,6 +1,5 @@
 # Import all the orm, so that Base has them before being
 # imported by Alembic
-from base_class import NpmLogBase
 import sys
 sys.path.insert(0, '/Users/badr/github_projects/common-codes')
-from databases_util.databases_schema.db_dbname_schema import *  # noqa
+from src.src.databases_util.databases_schema import *  # noqa
--- demo_db/test_schema/db_bases/base.py
+++ demo_db/test_schema/db_bases/base.py
@@ -1,7 +1,6 @@
 # Import all the orm, so that Base has them before being
 # imported by Alembic
 from base_class import MlflowBase  # noqa
-from base_class import DemoBase
 import sys
 sys.path.insert(0, '/Users/badr/github_projects/common-codes')
-from databases_util.databases_schema.mlflow_db_schema import *  # noqa
+from src.src.databases_util.databases_schema import *  # noqa
